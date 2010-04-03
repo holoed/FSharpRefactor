0db796226492741374705dfe0890c65c40f14fcf
holoed_cp <holoed@users.noreply.github.com>
2010-04-03 00:14:32 +0000

    Refactored using maybe monad

--- FSharpRefactor/ParserTests/ParserTests.fs
+++ FSharpRefactor/ParserTests/ParserTests.fs
@@ -4,18 +4,24 @@ open Tokenizer
 open Parser
 open NUnit.Framework
 
+type MaybeBuilder() =
+    member this.Bind (m, f) = Option.bind f m
+    member this.Return x = Some x
+
+let maybe = MaybeBuilder()
+
+let parse s = maybe  { let! x = tokenize s 
+                       let! y = parseCode x
+                       return y }
+
 [<TestFixture>]
 type ParserTests() =
     
     [<Test>]
     member this.BindingToValue() =
-        Assert.IsTrue (Some(Binding (Name (Identifier "x"), Literal (IntegerLiteral 42))) = 
-                            let (Some x) = tokenize "let x = 42" in 
-                            let ret = parseCode x in ret)
+        Assert.IsTrue (Some(Binding (Name (Identifier "x"), Literal (IntegerLiteral 42))) = parse "let x = 42")
 
     [<Test>]
     member this.BindingToArithmeticExpression() =
-        Assert.IsTrue (Some(Binding (Name (Identifier "x"), BinaryExpression (SymbolOp "*", Literal (IntegerLiteral 2), Literal (IntegerLiteral 3)))) = 
-                            let (Some x) = tokenize "let x = 2 * 3" in 
-                            let ret = parseCode x in ret)
+        Assert.IsTrue (Some(Binding (Name (Identifier "x"), BinaryExpression (SymbolOp "*", Literal (IntegerLiteral 2), Literal (IntegerLiteral 3)))) = parse "let x = 2 * 3")
                                                             
