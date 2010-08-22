eec428b13491f103a41dd531096bfb2babb5988f
holoed_cp <holoed@users.noreply.github.com>
2010-08-22 14:02:02 +0000

    Added Desugaring of functions from "let f x y z = body" to "let f = fun x -> fun y -> fun z -> body"

---
+++ FSharpRefactor/DeSugaring/DeSugarFunctions.fs
@@ -0,0 +1,15 @@
+﻿module DeSugarFunctions
+
+open Ast
+
+let rec desugarPat pat = 
+    match pat with
+    | PApp(PVar f, PVar x) -> (PVar f, fun body -> Lam(x, body))
+    | PApp(e1, PVar x) -> let (f, g) = desugarPat e1
+                          (f, fun body -> g (Lam(x, body)))
+
+let rec desugar exp = 
+    match exp with
+    | Let(pat, body, x) ->  let (f, g) = desugarPat pat
+                            Let(f, g body, x)
+    | _ -> exp
---
+++ FSharpRefactor/DeSugaringTests/DeSugarFunctionsTests.fs
@@ -0,0 +1,29 @@
+﻿module DeSugarFunctionsTests
+
+open NUnit.Framework
+open DeSugarFunctions
+open FSharpParser
+
+[<TestFixture>]
+type DeSugarFunctionsTests() =
+    [<Test>]
+    member this.``Desugar function "let f x = x" to "let f = fun x -> x"``() =
+        let (Some exp) = parseExp "let f x = x"
+        let (Some exp') = parseExp "let f = fun x -> x"
+        let exp'' = desugar exp
+        Assert.IsTrue ((exp' = exp''))
+
+    [<Test>]
+    member this.``Desugar function "let f x y = x" to "let f = fun x -> fun y -> x"``() =
+        let (Some exp) = parseExp "let f x y = x"
+        let (Some exp') = parseExp "let f = fun x -> fun y -> x"
+        let exp'' = desugar exp
+        Assert.IsTrue ((exp' = exp''))
+
+    [<Test>]
+    member this.``Desugar function "let f x y z = z" to "let f = fun x -> fun y -> fun z -> z"``() =
+        let (Some exp) = parseExp "let f x y z = z"
+        let (Some exp') = parseExp "let f = fun x -> fun y -> fun z -> z"
+        let exp'' = desugar exp
+        Assert.IsTrue ((exp' = exp''))
+        
