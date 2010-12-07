425bb646eceb844004463bb31fd9ccea1138fa40
holoed_cp <holoed@users.noreply.github.com>
2010-12-07 00:12:21 +0000

    Started working on find all references of an identifier

---
+++ FSharpRefactor/FSharpRefactor/ASTAnalysis.fs
@@ -0,0 +1,19 @@
+﻿module ASTAnalysis
+
+open Ast
+open AstCatamorphisms
+
+let findPos exp pos =  
+        let foldPat p = foldPat (fun (s,l) -> if (l = pos) then [Var (s,l)] else []) (fun l r -> l @ r) (fun x -> []) p
+        foldExp (fun (s, l) -> if (l = pos) then [Var (s,l)] else [])
+                (fun ps b -> (List.concat (List.map (fun p -> foldPat p) ps)) @ b)
+                (fun x y -> x @ y)
+                (fun op x y -> x @ y)
+                (fun p e1 e2 -> (foldPat p) @ e1 @ e2)
+                (fun x -> [])
+                (fun e t -> e)
+         exp
+
+
+let findAllReferences exps pos = 
+        List.fold (fun acc exp -> acc @ (findPos exp pos)) [] exps
---
+++ FSharpRefactor/FSharpRefactorTests/ASTAnalysisTests.fs
@@ -0,0 +1,26 @@
+﻿module ASTAnalysisTests
+
+open Ast
+open CompilerToAst
+open CompilerToAstTests
+open ASTAnalysis
+open NUnit.Framework
+
+[<TestFixture>]
+type ASTAnalysisTests() =
+
+    [<Test>]
+    member this.FindIdentifierByPos() =    
+                              //123456789
+        let ast = parseWithPos "let x = s"        
+        AssertAreEqual [Var ("x", loc(4,5,1,1))] (findAllReferences ast (loc(4,5,1,1)))
+        AssertAreEqual [Var ("s", loc(8,9,1,1))] (findAllReferences ast (loc(8,9,1,1)))
+
+    [<Test>]
+    [<Ignore("Working on this...")>]
+    member this.FindIdentifiersPosDescending() =    
+        let ast = parseWithPos "let x = x"        
+        AssertAreEqual [Var ("x", loc(4,5,1,1));Var ("x", loc(8,9,1,1))] (findAllReferences ast (loc(4,5,1,1)))
+       
+
+
