c67e64387e7c0dfcc378a6fc4f2db0a56fe010fa
holoed_cp <holoed@users.noreply.github.com>
2011-02-02 02:09:38 +0000

    Refactoring

--- FSharpRefactor/Ast/Catamorphism.fs
+++ FSharpRefactor/Ast/Catamorphism.fs
@@ -14,49 +14,49 @@ module AstCatamorphisms
 open Ast
 open ContinuationMonad
      
-let foldExp      varF 
-                 longVarF
-                 longVarSetF
-                 lamF 
-                 appF 
-                 letF 
-                 litF 
-                 tupleF 
-                 listF 
-                 expF 
-                 typesF 
-                 unionF 
-                 matchF
-                 clauseF 
-                 forEachF 
-                 yieldOrRetF 
-                 yieldOrRetFromF
-                 moduleF 
-                 openF 
-                 ifThenElseF
-                 dotIndexedSetF 
-                 dotIndexedGetF 
-                 recordDefF 
-                 recordInstF 
-                 recordFieldInstF 
-                 newF 
-                 noneF 
-                 classF 
-                 implicitConF 
-                 memberF 
-                 abstractSlotF 
-                 objExprF 
-                 doF 
-                 downcastF 
-                 upcastF 
-                 interfaceF 
-                 letBindingsF 
-                 abbrevF 
-                 tfunF 
-                 tIdentF
-                 tLongIdentF
-                 tvarF
-                 errorF 
+let foldExp      (varF: 'a -> 'b)
+                 (longVarF : 'b list -> 'b)
+                 (longVarSetF : 'b -> 'b -> 'b)
+                 (lamF : Pat<'a> list -> 'b -> 'b)
+                 (appF : 'b -> 'b -> 'b)
+                 (letF : IsLetRec -> Pat<'a> -> 'b -> 'b -> 'b)
+                 (litF : Literal -> 'b)
+                 (tupleF : 'b list -> 'b)
+                 (listF : 'b list -> 'b)
+                 (expF : 'b list -> 'c)
+                 (typesF : 'd list -> 'e)
+                 (unionF : string -> 'a list -> 'd)
+                 (matchF : 'b -> 'f list -> 'b)
+                 (clauseF : Pat<'a> -> 'b -> 'f)
+                 (forEachF : Pat<'a> -> 'b -> 'b -> 'b)
+                 (yieldOrRetF : 'b -> 'b)
+                 (yieldOrRetFromF : 'b -> 'b)
+                 (moduleF : string list -> 'e list -> 'e)
+                 (openF : string list -> 'e)
+                 (ifThenElseF : 'b -> 'b -> 'b option -> 'b)
+                 (dotIndexedSetF : 'b -> 'b list -> 'b -> 'b)
+                 (dotIndexedGetF : 'b -> 'b list -> 'b)
+                 (recordDefF : string -> 'a option list -> 'g list -> 'd)
+                 (recordInstF : 'h list -> 'b)
+                 (recordFieldInstF : 'a -> 'b -> 'h)
+                 (newF : 'i -> 'b -> 'b)
+                 (noneF : string -> 'd)
+                 (classF : string -> 'g list -> 'd)
+                 (implicitConF : Pat<'a> list -> 'g)
+                 (memberF : Pat<'a> -> 'b -> 'g)
+                 (abstractSlotF : string -> 'g)
+                 (objExprF : 'g list -> 'b)
+                 (doF : 'b -> 'b)
+                 (downcastF : 'b -> 'i -> 'b)
+                 (upcastF : 'b -> 'i -> 'b)
+                 (interfaceF : 'i -> 'g list -> 'g)
+                 (letBindingsF : 'b list -> 'g)
+                 (abbrevF : string -> 'i -> 'd)
+                 (tfunF : 'i -> 'i -> 'i)
+                 (tIdentF : 'a -> 'i)
+                 (tLongIdentF : 'i list -> 'i)
+                 (tvarF : 'i -> 'i)
+                 (errorF : unit -> 'b) 
                  decl =
   let rec LoopExp e =
           cont {  match e with
