a4a7cdd2881e93b6e57885ac87c3defc124345a7
holoed_cp <holoed@users.noreply.github.com>
2011-05-08 16:11:21 +0000

    Added Licence header where missing

--- FSharpRefactor/FSharpRefactorAddin/Rename/RenameDialog.xaml.cs
+++ FSharpRefactor/FSharpRefactorAddin/Rename/RenameDialog.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+// * copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound 
+// * by the terms of the Apache License, Version 2.0.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
 using System.Windows;
 using System.Windows.Controls;
 
--- FSharpRefactor/FSharpRefactorAddin/VsPackage/VsPkg.cs
+++ FSharpRefactor/FSharpRefactorAddin/VsPackage/VsPkg.cs
@@ -1,7 +1,15 @@
-using System;
-using System.ComponentModel.Design;
+// * **********************************************************************************************
+// * Copyright (c) Edmondo Pentangelo. 
+// *
+// * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
+// * copy of the license can be found in the License.html file at the root of this distribution. 
+// * By using this source code in any fashion, you are agreeing to be bound 
+// * by the terms of the Apache License, Version 2.0.
+// *
+// * You must not remove this notice, or any other, from this software.
+// * **********************************************************************************************
+
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
 using Microsoft.VisualStudio.Shell;
 
 namespace FSharpRefactorAddin.VsPackage
